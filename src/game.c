4afb953a7d17c4d73dc1f224c2a95810debadefb
LandarXT <104514709+LandarXT@users.noreply.github.com>
2022-08-25 15:07:57 +0100

    fixed the \

--- src/game.c
+++ src/game.c
@@ -7,7 +7,7 @@
 char input1[20];
 void startGame() {
 printf("Welcome to the game ");
-printf("Do you want to start the game? Type "y" to begin");
+printf("Do you want to start the game? Type \"y\" to begin");
     scanf(" %20[^\n]",  input1);
     printf("%s", input1);
     if(input1 == "y") {
