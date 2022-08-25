e95ac24e03cc1856a14d6a30b467793d6a651780
LandarXT <104514709+LandarXT@users.noreply.github.com>
2022-08-25 15:06:05 +0100

    fixed the semicolon of doom

--- src/game.c
+++ src/game.c
@@ -1,12 +1,13 @@
 
 
 #include "game.h"
+#include "chapter1.h"
 #include <stdio.h>
 #include <stdlib.h>
 char input1[20];
 void startGame() {
 printf("Welcome to the game ");
-printf("Do you want to start the game? Type "y" to begin")
+printf("Do you want to start the game? Type "y" to begin");
     scanf(" %20[^\n]",  input1);
     printf("%s", input1);
     if(input1 == "y") {
