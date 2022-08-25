2ac8091b3d28d8be8962479da43f7d3d1c5598f5
LandarXT <104514709+LandarXT@users.noreply.github.com>
2022-08-25 15:34:56 +0100

    Added choseToAskAgain variable

--- src/chapter1.c
+++ src/chapter1.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include "chapter1.h"
+int choseToAskAgain;
 int askTheQuestion() {
     printf("Nothing at all.");
 };
@@ -10,13 +11,18 @@ int soItBegins() {
     printf("Finished loading...\n");
     sleep(0.1);
     printf("\nNothing At All: A Text Adventure by LandarXT\n");
-    sleep(0.7);
+    sleep(2);
     printf("\nWelcome to my...\n ");
     printf("\nReply Options:\n");
     printf("1: Your what?\n");
+    printf("2: I'm sorry, can you say that again?\n");
     scanf("%i", &option);
     switch(option) {
         case 1:
+            askTheQuestion();
+            break;
+        case 2:
+            choseToAskAgain = 1;
             askTheQuestion();
             break;
         default:
