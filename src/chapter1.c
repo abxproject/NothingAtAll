96b2b5b2584b47981878ae6ab638af0f61a1543c
LandarXT <104514709+LandarXT@users.noreply.github.com>
2022-08-26 11:34:32 +0100

    Add decision string for scene 3

--- src/chapter1.c
+++ src/chapter1.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 #include "chapter1.h"
+#include <string.h>
 int choseToAskAgain;
 int choseCautiousApproach;
 int option = 0;
-
+char decidedscene3[];
 int scene3() {
    option = 0;
 
@@ -16,8 +17,10 @@ int scene3() {
        scanf("%i", &option);
        switch(option) {
            case 1:
+               strcpy(decidedscene3, "chose1.1");
                break;
            case 2:
+               strcpy(decidedscene3, "chose2.2");
                break;
        }
 
@@ -28,11 +31,17 @@ int scene3() {
        printf("\nReply Options:\n");
        printf("1: I'm just here, I guess.\n");
        printf("2: I never thought about it.\n");
+       printf("3: I'm not trying to be aggressive or anything\n");
        scanf("%i", &option);
        switch(option) {
            case 1:
+               strcpy(decidedscene3, "chose2.1");
                break;
            case 2:
+               strcpy(decidedscene3, "chose2.2");
+               break;
+           case 3:
+               strcpy(decidedscene3, "chose2.3");
                break;
        }
    }
