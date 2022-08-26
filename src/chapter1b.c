a1a5c98ed9eaf675cfff406adc67319a34bc38ce
LandarXT <104514709+LandarXT@users.noreply.github.com>
2022-08-26 16:38:54 +0100

    Add first gameover

--- src/chapter1b.c
+++ src/chapter1b.c
@@ -1,9 +1,41 @@
 #include <stdio.h>
 #include <string.h>
 #include "chapter1b.h"
-char decidedscene3[4];
+#include "main.c"
+char decidedscene3[4] = "1.0";
+int option = 0;
+int ending = 1;
+char commandInput[20];
+void endings() {
+    if(ending = 1) {
+        "Game OVER: Ending 1";
+    }
+    printf("Do you want to try again? (y or n)");
+    scanf("%20s", commandInput);
+    int test = strcmp(commandInput, "y");
+    if(test == 0) {
+        cmdTerminal();
+    }
+}
+
 void scene4() {
     //printf("Scene 4 success");
+   int strtest = strcmp(decidedscene3, "1.0");
+   if(strtest == 0) {
+       printf("\n That's not good enough for me. WHY ARE YOU HERE");
+       printf("\n \n Reply Options: ");
+       printf("\n 1: you run away in fear");
+       printf("\n 2: Uhh, I... chose to be here");
+       scanf("%i", &option);
+       switch(option) {
+           case 1:
+               ;
+       }
+
+
+
+   }
+
 }
 
 // this is the worst code to ever exist
