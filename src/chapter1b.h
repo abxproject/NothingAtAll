4da2630bc49036027c56e52b172144c69d1ef214
LandarXT <104514709+LandarXT@users.noreply.github.com>
2022-08-26 11:49:22 +0100

    Add functions into header

--- src/chapter1b.h
+++ src/chapter1b.h
@@ -1,5 +1,7 @@
 
 #ifndef NOTHINGATALL_CHAPTER1B_H
 #define NOTHINGATALL_CHAPTER1B_H
-void initChapter1b();
+void initChapter1b21();
+void initChapter1b22();
+void initChapter1b23();
 #endif //NOTHINGATALL_CHAPTER1B_H
